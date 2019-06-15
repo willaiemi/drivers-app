73bd5e1c9e3a69b42bdec3a8ac0f23b281e3feaa
Willian Duarte <willianm.cduarte@gmail.com>
2019-06-15 19:18:29 -0300

    connected to mongodb

--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
 // requiring express for routing
 const express = require('express')
+// requiring mongoose
+const mongoose = require('mongoose')
 
 // Creating app with express
 const app = express()
+
+// Starting database
+mongoose.connect('mongodb://localhost:27017/driverapi', {
+	useNewUrlParser: true
+})
 
 // First route
 app.get('/', (req, res) => {
