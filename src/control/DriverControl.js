806b3dd3927f6e0a96f8b130cd69b5806e0f3292
Willian Duarte <willianm.cduarte@gmail.com>
2019-06-15 20:08:24 -0300

    CRUD complete

--- src/control/DriverControl.js
+++ src/control/DriverControl.js
@@ -1,23 +1,48 @@
 // requiring mongoose
 const mongoose = require('mongoose')
+// preventing deprecated use
+mongoose.set('useFindAndModify', false)
 
+// get the driver model
 const Driver = mongoose.model('Driver')
 
 module.exports = {
 	async index(req, res) {
+		// get all drivers from the database
 		const drivers = await Driver.find()
 
+		//return all drivers in json format
 		return res.json(drivers)
 	},
 
 	async store(req, res) {
+		// create new driver in the database
 		const driver = await Driver.create(req.body)
 
+		// return in json format the newly created driver
 		return res.json(driver)
 	},
 	async show(req, res) {
+		// find driver by id
 		const driver = await Driver.findById(req.params.id)
 
+		// return the driver found
 		return res.json(driver)
+	},
+	async update(req, res) {
+		// find the driver by id and update it, returning the new driver
+		const driver = await Driver.findByIdAndUpdate(req.params.id, req.body, {
+			new: true
+		})
+
+		// return the newly updated driver
+		return res.json(driver)
+	},
+	async delete(req, res) {
+		// find a driver by id and remove it from the database
+		await Driver.findByIdAndRemove(req.params.id)
+
+		// return status 200
+		return res.send()
 	}
 }
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
+// requiring dependencies
 const express = require('express')
 const routes = express.Router()
 
+// get the driver controller
 const DriverControl = require('./control/DriverControl')
 
 // Routes
 routes.get('/drivers', DriverControl.index)
 routes.post('/drivers', DriverControl.store)
 routes.get('/drivers/:id', DriverControl.show)
+routes.put('/drivers/:id', DriverControl.update)
+routes.delete('/drivers/:id', DriverControl.delete)
 
+// export routes
 module.exports = routes
